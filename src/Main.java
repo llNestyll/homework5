aea05a7694f358c7f459724a193705ff8f39dd82
llNestyll <llnestyll@ro.ru>
2026-05-05 20:01:56 +0300

    сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,117 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
         }
+
+        int clientOS = 0;
+        if (clientOS == 0){
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        else if (clientOS == 1){
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+
+
+
+
+
+
+        int clientOSq = 0;
+        int clientDeviceYear = 2013;
+        if (clientOSq == 0 && clientDeviceYear > 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        else if (clientOSq == 0 && clientDeviceYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        else if (clientOSq == 1 && clientDeviceYear > 2015) {
+            System.out.println("Установите  версию приложения для Android по ссылке");
+        }
+        else  {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+
+
+
+
+
+        int year = 2023;
+        if (year > 1584 && ((year% 400 ==0) || (year% 4 == 0 && year % 100 != 0))){
+            System.out.println(year + " год является високосным");
+        }
+        else if (year > 1585){
+            System.out.println(year + " год не является високосным");
+        }
+        else {
+            System.out.println("год не может быть меньше 1585");
+        }
+
+
+
+
+        int deliveryDistance = 54;
+        int deliveryDays = 1;
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней "+deliveryDays);
+        }
+        else if (deliveryDistance > 20 && deliveryDistance <=60){
+            System.out.println("Потребуется дней "+(deliveryDays+1));
+        }
+        else if (deliveryDistance > 60 && deliveryDistance <=100){
+            System.out.println("Потребуется дней "+(deliveryDays+2));
+        }
+        else {
+            System.out.println("доставки нет");
+        }
+
+
+
+
+
+
+        char monthNumber = 13;
+        switch (monthNumber){
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("этот месяц принадлежит к сезону зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("этот месяц принадлежит к сезону весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("этот месяц принадлежит к сезону лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("этот месяц принадлежит к сезону осень");
+                break;
+            default:
+                System.out.println("месяца с таким номером не существует");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
